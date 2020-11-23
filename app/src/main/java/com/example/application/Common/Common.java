fb0d40e64610157dedd8318a9914cbdc8c6b5c3a
adrikar <orellanakaren30@gmail.com>
2020-11-22 20:35:21 -0400

    video4

--- app/src/main/java/com/example/application/Common/Common.java
+++ app/src/main/java/com/example/application/Common/Common.java
@@ -1,6 +1,8 @@
 package com.example.application.Common;
 
 
+import com.example.application.model.User;
+
 public class Common {
     public static String IS_LOGIN = "IsLogin";
     public static User currentUser;
